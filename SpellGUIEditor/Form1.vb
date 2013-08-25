888b78ec359a11054f54c03f01969fcea7491403
stoneharry <stoneharryjames@gmail.com>
2013-08-25 14:05:02 +0100

    I think I have fixed the targets flags and also added documentation about dispel types and mechanics to do.

--- SpellGUIEditor/Form1.vb
+++ SpellGUIEditor/Form1.vb
@@ -214,4 +214,57 @@ Public Class Form1
         Button5.Enabled = e
         Button6.Enabled = e
     End Sub
+
+
+    '    Enum DISPEL_TYPE
+    '{
+    '    DISPEL_ZGTRINKETS		= -1,
+    '    DISPEL_NULL				= 0,
+    '    DISPEL_MAGIC			= 1,
+    '    DISPEL_CURSE			= 2,
+    '    DISPEL_DISEASE			= 3,
+    '    DISPEL_POISON			= 4,
+    '    DISPEL_STEALTH			= 5,
+    '    DISPEL_INVISIBILTY		= 6,
+    '    DISPEL_ALL				= 7,
+    '    DISPEL_SPECIAL_NPCONLY	= 8,
+    '    DISPEL_FRENZY			= 9,
+    '};
+
+    '    Enum MECHANICS
+    '{
+    '    MECHANIC_NONE = 0,
+    '    MECHANIC_CHARMED, // 1
+    '    MECHANIC_DISORIENTED, // 2
+    '    MECHANIC_DISARMED, // 3
+    '    MECHANIC_DISTRACED, // 4
+    '    MECHANIC_FLEEING, // 5
+    '    MECHANIC_CLUMSY, // 6
+    '    MECHANIC_ROOTED, // 7
+    '    MECHANIC_PACIFIED, // 8
+    '    MECHANIC_SILENCED, // 9
+    '    MECHANIC_ASLEEP, // 10
+    '    MECHANIC_ENSNARED, // 11
+    '    MECHANIC_STUNNED, // 12
+    '    MECHANIC_FROZEN, // 13
+    '    MECHANIC_INCAPACIPATED, // 14
+    '    MECHANIC_BLEEDING, // 15
+    '    MECHANIC_HEALING, // 16
+    '    MECHANIC_POLYMORPHED, // 17
+    '    MECHANIC_BANISHED, // 18
+    '    MECHANIC_SHIELDED, // 19
+    '    MECHANIC_SHACKLED, // 20
+    '    MECHANIC_MOUNTED, // 21
+    '    MECHANIC_SEDUCED, // 22
+    '    MECHANIC_TURNED, // 23
+    '    MECHANIC_HORRIFIED, // 24
+    '    MECHANIC_INVULNARABLE, // 25
+    '    MECHANIC_INTERRUPTED, // 26
+    '    MECHANIC_DAZED, // 27
+    '    MECHANIC_DISCOVERY, // 28
+    '    MECHANIC_INVULNERABLE, // 29
+    '    MECHANIC_SAPPED, // 30
+    '    MECHANIC_ENRAGED, // 31
+    '    MECHANIC_END
+    '};
 End Class
--- SpellGUIEditor/Form5.Designer.vb
+++ SpellGUIEditor/Form5.Designer.vb
@@ -28,7 +28,7 @@ Partial Class Form5
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(378, 362)
+        Me.Button2.Location = New System.Drawing.Point(378, 396)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(85, 35)
         Me.Button2.TabIndex = 1
@@ -38,19 +38,17 @@ Partial Class Form5
         'CheckedListBox1
         '
         Me.CheckedListBox1.FormattingEnabled = True
-        Me.CheckedListBox1.Items.AddRange(New Object() {"Player or target ? Might also be none-individual target", "? (Not used in 3.3.5a on it's own)", "? (Not used in 3.3.5a on it's own)", "? (Not used in 3.3.5a on it's own)", "? (Not used in 3.3.5a on it's own)", "Enchantment (Item in inventory and equipment)", "Point-Blank AoE", "Targetable AoE ", "Targetable AoE ?(Some Stealth-revealing spell AoE has it)", "Teleport ? Seems general based (Seems to handle every Player in AoE individually)" & _
-                "", "Player corpses selectable ? Used for looting insignias.", "Revives every player in Point-Blank AoE (Spirit Revival)", "Used for skinning (Target corpses ?)", "Spawns something on caster spot ? (Used in a spell that drops object at caster po" & _
-                "sition)", "? (Not used in 3.3.5a on it's own)", "? (Not used in 3.3.5a on it's own)", "Target chests ? Used for Lock-opening spells", "? (Used with non-referenced, passive spells)", "? (Not used in 3.3.5a on it's own)", "Glyphs (Maybe change how another spell works ?)"})
+        Me.CheckedListBox1.Items.AddRange(New Object() {"NONE            ", "UNUSED_1        ", "UNIT            someone", "UNIT_RAID       someone in raid", "UNIT_PARTY      someone in party", "ITEM            item enchantment", "SOURCE_LOCATION point blank AoE", "DEST_LOCATION target AoE", "UNIT_ENEMY      target dead players", "UNIT_ALLY       target allies", "CORPSE_ENEMY target dead enemies   ", "UNIT_DEAD       target dead", "GAMEOBJECT      spawn game object", "TRADE_ITEM      ", "STRING          ", "GAMEOBJECT_ITEM ", "CORPSE_ALLY     ", "UNIT_MINIPET    ", "GLYPH_SLOT      ", "DEST_TARGET     ", "UNUSED20        ", "UNIT_PASSENGER"})
         Me.CheckedListBox1.Location = New System.Drawing.Point(12, 12)
         Me.CheckedListBox1.Name = "CheckedListBox1"
-        Me.CheckedListBox1.Size = New System.Drawing.Size(451, 344)
+        Me.CheckedListBox1.Size = New System.Drawing.Size(451, 378)
         Me.CheckedListBox1.TabIndex = 2
         '
         'Form5
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(475, 406)
+        Me.ClientSize = New System.Drawing.Size(473, 441)
         Me.Controls.Add(Me.CheckedListBox1)
         Me.Controls.Add(Me.Button2)
         Me.MaximizeBox = False
